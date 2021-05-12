6f16b38227272716a110351e4b668553e421b23c
mdepeiza1 <77159641+mdepeiza1@users.noreply.github.com>
2021-05-12 11:35:43 -0400

    Added win conditions
    Game flow may need to be refined

--- TicTacToe/wwwroot/js/script.js
+++ TicTacToe/wwwroot/js/script.js
@@ -25,5 +25,44 @@ function handlePlayerChange() {
         currentPlayer = "X";
     }
     statusDisplay.innerHTML = currentPlayerTurn();
+    handleResultValidation();
+}
+
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8]
+];
+function handleResultValidation() {
+    let roundWon = false;
+    for (let i = 0; i <= 7; i++) {
+        const winCondition = winningConditions[i];
+        let a = gameState[winCondition[0]];
+        let b = gameState[winCondition[1]];
+        let c = gameState[winCondition[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if ((a === 'X' && b === 'X' && c === 'X') ||
+            (a === 'O' && b === 'O' && c === 'O')) {
+            roundWon = true;
+            break
+        }
+    }
+    if (roundWon) {
+        if (currentPlayer === 'X') {
+            statusDisplay.innerHTML = 'O has won!';
+        }
+        if (currentPlayer === 'O') {
+            statusDisplay.innerHTML = 'X has won!';
+        }
+        gameActive = false;
+        return;
+    }
 }
 document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
